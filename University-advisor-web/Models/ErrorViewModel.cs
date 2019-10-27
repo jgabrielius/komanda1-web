6eac0da23e429916d9172d9e9eaa063a5ceb3de4
Gabrielius Jokūbauskas <jgabrielius@gmail.com>
2019-10-27 14:46:53 +0200

    initial commit

---
+++ University-advisor-web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace University_advisor_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

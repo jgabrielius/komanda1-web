c0c6fcc0a2a3f6581feba8abf20c6d3296446ed8
Jaroslav Avižen <avizen.j@gmail.com>
2019-10-27 18:22:35 +0200

    Added user model and registration controller.

---
+++ University-advisor-web/Controllers/RegistrationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using University_advisor_web.Models;
+
+namespace University_advisor_web.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new UserModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(UserModel model)
+        {
+            //if (ModelState.IsValid)
+            //{
+            //    // TODO: When model state is valid, new user has to be created
+            //    return View(model);
+            //}
+            return View("../Registration/UserCreated", model);
+        }
+    }
+}
---
+++ University-advisor-web/Models/UserModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace University_advisor_web.Models
+{
+    public class UserModel
+    {
+        public UserModel()
+        {
+            Universities = new List<SelectListItem>
+            {
+                // TODO: Needs to be fetched from DB
+                new SelectListItem("Vilnius university", "Vilnius university"),
+                new SelectListItem("ISM", "ISM"),
+                new SelectListItem("Vilnius Gediminas Technical University", "Vilnius Gediminas Technical University")
+            };
+            Statuses = new List<SelectListItem>
+            {
+                new SelectListItem("Student", "Student"),
+                new SelectListItem("Lecturer", "Lecturer"),
+                new SelectListItem("Graduate", "Graduate")
+            };
+        }
+
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string University { get; set; }
+        public string Status { get; set; }
+        public List<SelectListItem> Universities { get; set; }
+        public List<SelectListItem> Statuses { get; set; }
+        public string Password { get; set; }
+    }
+}

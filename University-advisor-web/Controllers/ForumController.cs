a4c675669eb658c437123d3bdbe1eff52ff72bda
Jaroslav Avižen <avizen.j@gmail.com>
2019-11-09 19:55:07 +0200

    Started to develop q&a section.

---
+++ University-advisor-web/Controllers/ForumController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using University_advisor_web.Models;
+
+namespace University_advisor_web.Controllers
+{
+    public class ForumController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Question()
+        {
+            var userQuestion = new QuestionModel();
+            return View(userQuestion);
+        }
+
+        [HttpPost]
+        public IActionResult Question(QuestionModel userQuestion)
+        {
+            return View(userQuestion);
+        }
+    }
+}
---
+++ University-advisor-web/Models/QuestionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace University_advisor_web.Models
+{
+    public class QuestionModel
+    {
+        public string Question { get; set; }
+        public string Message { get; set; }
+
+    }
+}

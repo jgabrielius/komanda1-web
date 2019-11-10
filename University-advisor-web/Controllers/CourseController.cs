73b90e0ba22915c43e79d7e4a896a3199a2ef5b4
MartinCepulionis <MartinCepulionis@users.noreply.github.com>
2019-11-10 22:13:29 +0200

    Merge conflict fix

---
+++ University-advisor-web/Controllers/CourseController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using University_advisor_web.Models;
+
+namespace University_advisor_web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        // GET: api/Course
+        [HttpGet]
+        public IEnumerable<AutoCompleteItemModel> Get()
+        {
+            var listOfItems = new List<AutoCompleteItemModel>();
+            var listOfCourses = new UniversityModel().GetAllCoursesWithUniversityNames();
+            foreach (var course in listOfCourses)
+            {
+                listOfItems.Add(
+                    new AutoCompleteItemModel(
+                        $"{course["program"].ToString()} ({course["name"].ToString()})",
+                        "CourseReview",
+                        Convert.ToInt32(course["studyProgramId"].ToString()))
+                    );
+            }
+            return listOfItems;
+        }
+    }
+}
---
+++ University-advisor-web/Models/AutoCompleteItemModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace University_advisor_web.Models
+{
+    public class AutoCompleteItemModel
+    {
+        public string Name { get; set; }
+        public string AspAction { get; set; }
+        public int ItemId { get; set; }
+        public AutoCompleteItemModel(string name, string aspAction, int id) 
+        {
+            Name = name;
+            AspAction = aspAction;
+            ItemId = id;
+        }
+    }
+}

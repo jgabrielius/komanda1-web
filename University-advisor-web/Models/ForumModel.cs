14d59c885379f6a9fb84238214cffa5160299d38
Jaroslav Avižen <avizen.j@gmail.com>
2019-11-17 22:01:49 +0200

    Changed the way replies and questions are saved.

---
+++ University-advisor-web/Models/ForumModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace University_advisor_web.Models
+{
+    public class ForumModel
+    {
+        public int userId { get; set; }
+        public int questionId { get; set; }
+        public string question { get; set; }
+        public string message { get; set; }
+        public int answerId { get; set; }
+        public int userIdReply { get; set; }
+        public string answer { get; set; }
+        public ForumModel()
+        {
+
+        }
+
+        public ForumModel(int questionId)
+        {
+            this.questionId = questionId;
+            var sqlQuestion = SqlDriver.Row($"SELECT question, message, userId FROM questions WHERE questionId = {questionId};");
+            question = sqlQuestion["question"].ToString();
+            userId = Int32.Parse(sqlQuestion["userId"].ToString());
+            message = sqlQuestion["message"].ToString();
+        }
+
+        public void SaveQuestion()
+        {
+            Random random = new Random();
+            int id = random.Next();
+            questionId = id;
+            SqlDriver.Execute("INSERT INTO questions (userId,questionId,question,message) " +
+                "values (@0,@1,@2,@3)", new ArrayList() { userId, questionId, question, message});
+        }
+
+        public void SaveReply()
+        {
+            Random random = new Random();
+            int id = random.Next();
+            answerId = id;
+            SqlDriver.Execute("INSERT INTO answers (userId,answerId,questionId,answer) " +
+                "values (@0,@1,@2,@3)", new ArrayList() { userIdReply, answerId, questionId, answer });
+        }
+
+        public List<Dictionary<string, object>> GetAllQuestions()
+        {
+            return SqlDriver.Fetch($"SELECT * FROM questions");
+        }
+        public List<Dictionary<string, object>> GetAllReplies()
+        {
+            return SqlDriver.Fetch($"SELECT * FROM answers WHERE questionId={questionId}");
+        }
+    }
+
+}

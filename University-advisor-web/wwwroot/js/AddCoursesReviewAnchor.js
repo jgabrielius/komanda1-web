703623f9c1cb98e44d6ecbe0dece57dd5dcb84ea
MartinCepulionis <MartinCepulionis@users.noreply.github.com>
2019-12-08 15:24:25 +0200

    University logo images.

--- University-advisor-web/wwwroot/js/AddCoursesReviewAnchor.js
+++ University-advisor-web/wwwroot/js/AddCoursesReviewAnchor.js
@@ -1,13 +1,12 @@
-﻿const courses = JSON.parse(document.getElementById('value1').value);
+﻿const courseId = JSON.parse(document.getElementById('value1').value);
 const userCourseId = JSON.parse(document.getElementById('value2').value);
 
-courses.forEach(course => {
-    if (course === userCourseId) {
-        let reviewTableRow = document.getElementById(`${course}`);
-        let aTag = document.createElement('a');
-        aTag.setAttribute('class', "btn btn-primary");
-        aTag.setAttribute('href', `/Review/CourseReview/${userCourseId}`);
-        aTag.innerText = "Review";
-        reviewTableRow.appendChild(aTag);
-    }
-})
+if (courseId === userCourseId) {
+
+    let reviewDiv = document.getElementById("reviewAnchorDiv");
+    let aTag = document.createElement('a');
+    aTag.setAttribute('class', "btn btn-outline-primary");
+    aTag.innerText = "Review course";
+    aTag.setAttribute('href', `/Review/CourseReview/${userCourseId}`);
+    reviewDiv.appendChild(aTag);
+}

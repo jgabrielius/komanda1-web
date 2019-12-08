a8d679899d2be042de3111808aab05929b19d56a
Gabrielius Jokūbauskas <jgabrielius@gmail.com>
2019-12-08 22:15:55 +0200

    Add Entity Framework integration

---
+++ University-advisor-web/EntityFramework/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace University_advisor_web.EntityFramework
+{
+    public class Log
+    {
+        public int LogId { get; set; }
+        public string Query { get; set; }
+
+        public string Date { get; set; }
+    }
+}
--- University-advisor-web/Tools/SqlDriver.cs
+++ University-advisor-web/Tools/SqlDriver.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Collections;
 using Microsoft.Data.SqlClient;
 using System.Data;
+using University_advisor_web.EntityFramework;
 
 namespace University_advisor_web
 {
@@ -57,7 +58,7 @@ namespace University_advisor_web
             }
             catch (SQLiteException e)
             {
-                //TODO add error logging
+               
                 return null;
             }
         }
@@ -119,7 +120,17 @@ namespace University_advisor_web
                 command = new SQLiteCommand(sql, dbConnection);
             }
 
-            //TODO add logging and error handling
+            using (var dbContext = new MyContext())
+            {
+                dbContext.Database.EnsureCreated();
+                dbContext.Logs.AddRange(new Log
+                    {
+                        Query = command.CommandText,
+                        Date = DateTime.Now.ToString()
+            });
+                dbContext.SaveChanges();
+            }
+
             command.ExecuteNonQuery();
             command.Dispose();
             dbConnection.Close();
